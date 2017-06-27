afb376bb3eba55f517a2ab69c5f804184d669a8d
the-james-burton <the.james.burton@gmail.com>
2017-06-27 08:40:35 +0100

    updated perf file locations

--- src/main/java/performance/Main.java
+++ src/main/java/performance/Main.java
@@ -19,6 +19,8 @@ public class Main implements Runnable {
   @Override
   public void run() {
     logger.info("perf.ms.config:{}", System.getProperty("perf.ms.config"));
+    logger.info("java.io.tmpdir:{}", System.getProperty("java.io.tmpdir"));
+
     engine.runEngine();
   }
 
